6d9cf315bc17b987d18f65926c847c5eaed8cc79
Ronald Schmidt <ronald.schmidt@sparwelt.de>
2016-02-15 16:54:07 +0100

    add public method trackPagePing

--- src/js/tracker.js
+++ src/js/tracker.js
@@ -1457,6 +1457,19 @@
 			},
 
 			/**
+			 * Log activity on this page
+			 *
+			 * @param string customTitle
+			 * @param object Custom context relating to the event
+			 * @param object contextCallback Function returning an array of contexts
+			 */
+			trackPagePing: function (customTitle, context, contextCallback) {
+				trackCallback(function () {
+						logPagePing(customTitle, context, contextCallback);
+				});
+			},
+
+			/**
 			 * Track a structured event happening on this page.
 			 *
 			 * Replaces trackEvent, making clear that the type
